2e5251ad543675fc56fa5bc5220d5db9391f7768
mohamed osama <oooomohamed421@gmail.com>
2025-08-26 21:04:38 +0300

    Edit reresh token model

---
+++ CompanySmartChargingSystem.Domain/Entities/RefreshTokenModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanySmartChargingSystem.Domain.Entities
+{
+    [Owned]
+    public class RefreshTokenModel
+    {
+        public string Token { get; set; }
+        public bool isNew { get; set; }
+        public DateTime Expiration { get; set; }
+        public bool IsActive => DateTime.UtcNow < Expiration;
+        public DateTime createdOn { get; set; } = DateTime.UtcNow;
+        public DateTime? revokedOn { get; set; } = null;
+
+
+    }
+}
